be091cf8f2b8add3911ca3d443ba8cd81f2e6e1a
Joe Blurton <joe@doitdevon.com>
2020-06-17 11:06:10 +0100

    Added icons and updated bulma

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
-        title: 'Gatsby + Netlify CMS Starter',
-        description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+        title: 'Devon Stories',
+        description: 'Tales from deepest Devonshire.',
     },
     plugins: [
         'gatsby-plugin-react-helmet',
